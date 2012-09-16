05ed05114587fda440d18612f134ab134d8cb396
Bradley Hart <brad@homeubuntu.(none)>
2012-09-16 22:05:37 +0100

    inital commit with tempate from ccs templates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MosqueSite::Application.config.secret_token = 'c39e6a242f2f9602c25bd9a7489152c0e33d51a6b8896f0609aaf9aca550b4af17533f289c178b9e9db3c1903534e11d61c4f5da34b5e4ac10ad3a0d3c6e87f8'
