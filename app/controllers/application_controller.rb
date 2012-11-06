86a2bb8f7700ed50a40a918b700d0e5726a27e9e
Bradley Hart <bradley.m.hart@gmail.com>
2012-11-06 20:19:22 +0000

    added entries to all the pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :get_entries
 
   def index
 
+  end
+
+  def get_entries
+    @recent_entries = Entry.all(:order => "id desc", :limit => 5)
   end
 
 end
